7b4e91c1a68e758cde7910c680dc4a1412f0303d
NahuelDiaz11 <nahuel.kp2010@hotmail.es>
2023-01-03 21:53:21 -0300

    Carga de imagenes

--- app/Http/Livewire/Categories.php
+++ app/Http/Livewire/Categories.php
@@ -3,32 +3,77 @@
 namespace App\Http\Livewire;
 
 use App\Models\Category;
+use App\Models\Image;
 use Livewire\Component;
 
 class Categories extends Component
 {
     //propiedades publicas
-    public $form = false, $name='', $selected_id=0, $photo='';
-    public $action = 'Listado', $componentName= 'Categorias', $search='';
+    public $form = false, $name = '', $selected_id = 0, $photo = '';
+    public $action = 'Listado', $componentName = 'Categorias', $search = '';
     private $pagination = 4;
-    protected $paginationTheme='tailwind';
+    protected $paginationTheme = 'tailwind';
 
 
     public function render()
     {
-        if(strlen($this->search)>0)
-        $info = Category::where('name','like', "%{$this->search}%" )->paginate($this->pagination);
+        if (strlen($this->search) > 0)
+            $info = Category::where('name', 'like', "%{$this->search}%")->paginate($this->pagination);
         else
-        $info = Category::orderBy('name','asc')->paginate($this->pagination);
+            $info = Category::orderBy('name', 'asc')->paginate($this->pagination);
 
 
         return view('livewire.categories.component', [
-            'categories'=>$info
+            'categories' => $info
         ])
-        ->layout('layouts.theme.app');
+            ->layout('layouts.theme.app');
     }
 
-    public function Edit(Category $catgory){
+    public function Edit(Category $catgory)
+    {
+    }
 
+    public function Store()
+    {
+        //actualizar o crear nuevas categorias
+        sleep(1);
+        $this->validate(Category::rules($this->selected_id), Category::$messages);
+
+
+        $category = Category::updatOrCreate([
+            ['id' => $this->selected_id],
+            ['name' => $this->name]
+        ]);
+
+        //si tenemos una nueva imagen se elimina la previa
+        if (!empty($this->photo)) {
+            //se elimina las imagenes de la carpeta
+            $tempImg = $category->imag->file;
+            if ($tempImg != null && file_exists('storage/categories/' . $tempImg)) {
+                unlink('storage/categoris/', $tempImg);
+            }
+
+            //se elimina la relacion con imagenes de la base de datos
+            $category->image()->delate();
+            //generamos un nombre de archivo unico
+            $customFileName = uniqid() . '_.' . $this->photo->extension();
+            //guardamos imagen en esta ruta con el nombre unico
+            $this->photo->storeAs('public/categories', $customFileName);
+
+            //creamos registro en la bd
+            $img = Image::create([
+                'model_id' => $category->id,
+                'model_type' => 'App\Models\Category',
+                'file' => $customFileName
+
+            ]);
+            //guardar relacion
+            $category->image()->save($img);
+        }
+
+        //mandamos feedback
+        $this->noty($this->selected_id < 1 ? 'Categoria registrada' : 'Categoria actualizada', 'noty', false, 'close-modal');
+
+        $this->resetUI();
     }
 }
---
+++ resources/views/livewire/form.blade.php
@@ -0,0 +1,47 @@
+<div class="intro-y col-span-12">
+    <div class="intro-y box">
+        <div class="flex flex-col sm-flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+            <h2 class="font-medium text base mr-auto">
+                {{ $componentName }} |<span class="font-normal">{{ $action }}</span>
+            </h2>
+        </div>
+
+        <div class="p-5">
+            <div class="preview">
+                {{-- apenas abre el formulario se abre el teclado virtual con alpine, con el nombre first que esta en la caja de texto --}}
+                <div x-data="{}" x-init="setTimeout(() => { $refs.first.focus() }, 900 )">
+                    <label class="form-label">Nombre</label>
+                    {{-- en el caso que no este completo con la propiedad border theme pinta de rojo el input --}}
+                    <input type="text" wire:model="name" x-ref="first" id="categoryName"
+                    class="form-control kioskboard {{$errors->first('name') ? "border-theme-6" : ""}}"
+                    placeholder="ingresa la descripcion"
+                    >
+
+                    @error('name')
+                    <x-alert msg="{{$message}}" />
+                    @enderror
+                </div>
+                <div class="mt-3">
+                    <label class="form-label">Imagen</label>
+                    {{-- aceptamos imagenes png, jpeg y jpg --}}
+                    <input type="file" wire:model="photo" accept="image/x-png,image/jpeg,image/jpg" class="form-control">
+                </div>
+
+                {{-- cuando el user selecciona una img la podemos previsualizar --}}
+                <div class="div-mt-3" id="avatar">
+                    @if($photo)
+                    <img class="rounder-lg mb-5 recent-product-img" src="{{ $photo->temporaryUrl() }}" alt="" width="150" >
+                    @endif
+
+                </div>
+
+                <div class="mt-5">
+                    <x-back />
+
+                    <x-save />
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
