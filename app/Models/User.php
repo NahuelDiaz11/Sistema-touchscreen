12257e351b0b185ad7da9985e9ceba630b097a90
nahuel diaz <nahuel.kp2010@hotmail.es>
2023-02-13 11:57:48 -0300

    Validaciones de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,61 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public static function rules()
+    {
+        if($id<=0){
+            return[
+                'name' => 'required|min:3|max:50|string|unique:users',
+                'email'=> 'required|email|unique:users',
+                'password'=> 'required|min:3',
+                'profile'=>'required|not_in:eleger'
+            ];
+        }else{
+            return[
+                'name' => "required|min:3|max:50|string|unique:users,name,{$id}",
+                'email'=> 'required|email|unique:users',
+                'password'=> 'required|min:3',
+                'profile'=>'required|not_in:eleger'
+            ];
+        }
+    }
+
+    public static $messages =[
+        'name.required' => 'Nombre requerido',
+        'name.min' => 'El nombre debe tener al menos 3 caracteres',
+        'name.max' => 'EL nombre puede tener maximo 50 caracteres',
+        'name.unique' => 'El nombre ya existe',
+        'email.required' => 'Email requerido',
+        'email.email' => 'El email es invalido',
+        'password.required' => 'Contraseña requerida',
+        'password.min' => 'La Contraseña debe tener al menos 3 caracteres',
+        'profile.required' => 'Perfil requerido',
+        'profile.not_in' => 'Selecciona un perfil valido'
+
+    ];
+
+    public function image()
+    {
+        //falta logica de avatars
+
+        return $this->morphOne(Image::class, 'model')->withDefault();
+    }
+
+    //accesores y mutadores
+    //obtenemos avatares
+    public function getAvatarAttribute()
+    {
+        $img=$this->image->file;
+        if($img !=null){
+            if(file_exists('storage/avatars/'.$img))
+            return 'storage/avatars'.$img;
+            else
+            return 'storage/default_avatar.png/';
+        }
+
+        return 'storage/default_avatar.png';
+    }
+
     //relaciones
     public function sales()
     {
